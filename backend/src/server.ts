0a19e8cd132b0cfefcc75234afc9e8a5ea199c4a
taha <mohammad.taha.jam@gmail.com>
2026-09-15 01:12:29 +0330

    feat:addded emoji for server running

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,5 +7,5 @@ const port = Number(process.env.PORT ?? 3000);
 app.use(express.json());
 
 app.listen(port, "127.0.0.1", () => {
-  console.log(`Server running at http://127.0.0.1:${port}`);
+  console.log(`Server running at http://127.0.0.1:${port} 👽🛸🛰️`);
 });
