5787a7686dce5bf496df27c98127bf77e3e23565
taha <mohammad.taha.jam@gmail.com>
2026-09-15 21:06:56 +0330

    feat:add heath and info endpoint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,25 @@
 import "dotenv/config";
 import express from "express";
+import { ok } from "node:assert";
 
 const app = express();
 const port = Number(process.env.PORT ?? 3000);
 
 app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    message: "server is running",
+  });
+console.log("Incoming request:", req.method, req.path);});
+
+app.get("/api/info", (req, res) => {
+  res.status(200).json({
+    status: "team task manager",
+    message: "0.1.0",
+  });
+console.log("Incoming request:", req.method, req.path);});
 
 app.listen(port, "127.0.0.1", () => {
   console.log(`Server running at http://127.0.0.1:${port} 👽🛸🛰️`);
